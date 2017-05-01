eee3fe905423001f4a801a3c80e25adae1680193
jind11 <jindi15@mit.edu>
2017-04-30 23:10:21 -0400

    update gram extraction

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,8 @@ from stop_words import get_stop_words
 from multiprocessing import Pool
 import time
 import os
+import codecs
+import re
 
 tokenizer = RegexpTokenizer(r'\w+') # create nltk tokenizer
 en_stop = get_stop_words('en') # create English stop words list
@@ -49,12 +51,15 @@ def sentence_segment(drugname):
 	drug_file = open(filepath, 'r')
 	drug_raw = drug_file.read()
 	drug_file.close()
-
+	
+	drug_raw = drug_raw.replace('e.g.', '')
+	drug_raw = re.sub('\d+[.]*\d*[%]*', '', drug_raw)
+	drug_raw = drug_raw.replace('()', '')
 	drug_raw = drug_raw.replace('.', '.\n')
-	drug_raw = os.linesep.join([s.lstrip(' ') for s in drug_raw.splitlines() if s])
+	drug_raw = os.linesep.join([s.lstrip(' ').decode('utf-8') for s in drug_raw.splitlines() if s and s != ' '])
 	filepath = 'drugs/preprocessed/{}.txt'.format(drugname)
 	with open(filepath, 'w') as file:
-		file.write(drug_raw)
+		file.write(drug_raw.encode("ascii", errors='ignore'))
 		file.close()
 
 
@@ -62,17 +67,20 @@ if __name__ == "__main__":
 	drug_name_file = open('./drugs_com_web_names.txt','r')
 	drug_names = drug_name_file.readlines()
 	drug_names = [drug_name.replace('\n','') for drug_name in drug_names]
-	pool = Pool()
-	result = pool.map_async(sentence_segment, drug_names, chunksize=int(len(drug_names)/8))
-	while (True):
-	  if (result.ready()): break
-	  remaining = result._number_left
-	  print ("Waiting for", remaining, "tasks to complete...")
-	  time.sleep(1)
-	print ('All tasks completed!')
-	pool.close()
-	for drugname in drug_names:
-		check_parse(drugname)
+	# sentence_segment('all day allergy')
+	# pool = Pool()
+	# result = pool.map_async(sentence_segment, drug_names, chunksize=int(len(drug_names)/8))
+	# while (True):
+	#   if (result.ready()): break
+	#   remaining = result._number_left
+	#   print ("Waiting for", remaining, "tasks to complete...")
+	#   time.sleep(1)
+	# print ('All tasks completed!')
+	# pool.close()
+	for drug_name in drug_names:
+		sentence_segment(drug_name)
+	# for drugname in drug_names:
+	# 	check_parse(drugname)
 
 	# with open('drugs/wrongName.txt', 'w') as file:
 	# 	file.write(str(wrong_drugname))
